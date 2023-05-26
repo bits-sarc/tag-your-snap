ca96a9f7fe284ebc4422528b34871531555b04ad
Himanshu Kumar <kumarh664@gmail.com>
2023-05-27 04:44:23 +0530

    added permission classes

--- server/snaps/views.py
+++ server/snaps/views.py
@@ -1,15 +1,16 @@
-from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.parsers import MultiPartParser ,FormParser
-from .serializers import SnapSerializer ,BatchSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
+from .serializers import SnapSerializer, BatchSerializer
 from .models import Batch
 
 
 # Create your views here.
-'''
+"""
 Snap View
 
 post request
@@ -19,25 +20,28 @@ rename file to branch code .extension
 get request
 send back list of snaps
 branch codes
-'''
-class SnapView(APIView):
-    parser_classes=(MultiPartParser,FormParser)
+"""
 
-    def post(self,request,format=None,*args,**kwargs):
+@api_view(['GET', 'POST'])
+@permission_classes([IsAdminUser])
+class SnapView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, format=None, *args, **kwargs):
         batch_serializer = BatchSerializer(data=request.data)
         if batch_serializer.is_valid():
             batch_serializer.save()
-            return Response(batch_serializer.data ,status=status.HTTP_201_CREATED)
+            return Response(batch_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(batch_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, format=None):
         batch = Batch.objects.all()
-        batch_serializer = BatchSerializer(batch,many=True)
+        batch_serializer = BatchSerializer(batch, many=True)
         return Response(batch_serializer.data)
 
 
-'''
+"""
 SnapDetailsView
 get request : batch code
 snap id
@@ -49,4 +53,4 @@ batch code
 post request
 snap id
 
-'''
+"""
