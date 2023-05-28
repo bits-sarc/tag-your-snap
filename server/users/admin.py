d4b0d06d0ddfe7a6d229c160619837b626e09fc2
Himanshu Kumar <kumarh664@gmail.com>
2023-05-28 19:35:39 +0530

    admin panel custom user

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import StudentProfile, Dimension
+from .models import StudentProfile, Dimension, CustomUser
 
 # Register your models here.
+admin.site.register(CustomUser)
 admin.site.register(StudentProfile)
 admin.site.register(Dimension)
