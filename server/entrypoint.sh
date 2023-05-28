177d7f306f42d2cebebfb1b701adfd421025989e
Himanshu Kumar <kumarh664@gmail.com>
2023-05-28 16:51:15 +0530

    fix migrations

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -12,7 +12,9 @@ fi
 
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
-python manage.py makemigrations --noinput 
+python manage.py makemigrations core --noinput 
+python manage.py makemigrations users --noinput 
+python manage.py makemigrations snaps --noinput 
 python manage.py migrate --noinput 
 python manage.py migrate --run-syncdb
 python manage.py collectstatic --noinput
