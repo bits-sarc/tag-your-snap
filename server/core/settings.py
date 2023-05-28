a11f5a406c32007f76ae9a78bd5b61948e7286dc
Himanshu Kumar <kumarh664@gmail.com>
2023-05-28 19:29:32 +0530

    add allowed host

--- server/core/settings.py
+++ server/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', '20.68.16.151']
 
 
 # Application definition
