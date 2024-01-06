dda368faccb45a78ca6f3420ee859d4241f144e4
Himanshu Kumar <kumarh664@gmail.com>
2024-01-06 21:52:33 +0530

    remove tutorial residue

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,20 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import { createContact, getContacts } from '../contacts';
-// import { Contact } from '../types/contact';
 import NavBar from '../components/NavBar';
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
-}
-
-export async function action() {
-  const contact = await createContact();
-  return { contact };
-}
-
 export default function Root() {
-  // const { contacts } = useLoaderData() as { contacts: Contact[] };
   return (
     <div className='bg-black text-white'>
       <NavBar />
