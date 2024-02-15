a64c8573a925a9bf6d6a4c3b3bae633552f8b96a
Rakshit2622 <rakshit.sakhuja345@gmail.com>
2024-02-15 22:31:41 +0530

    added import export in admin

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import UserProfile, Location
+from import_export.admin import ImportExportModelAdmin, ExportMixin
+from import_export import resources
+
+
+class UserProfileResource(resources.ModelResource):
+    class Meta:
+        model = UserProfile
+        fields = ["user__email", "bits_id"]
+        export_order = ["user__email", "bits_id"]
+
+
+class UserProfileAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = UserProfileResource
+
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Location)
