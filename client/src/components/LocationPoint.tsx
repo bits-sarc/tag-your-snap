5e8ebeace3fc0e4827fce9541cd034b9721e2c13
Himanshu Kumar <33803331+Zendovo@users.noreply.github.com>
2024-02-23 23:16:18 +0530

    add more colors

--- client/src/components/LocationPoint.tsx
+++ client/src/components/LocationPoint.tsx
@@ -4,13 +4,13 @@ export default function LocationPoint({ location, currentRow, id, onClick }: { l
     const percentX = location.x;
     const percentY = location.y;
 
-    let color = ["bg-amber-300", "bg-lime-300", "bg-teal-300", "bg-indigo-300", "bg-rose-300"]
+    let color = ["bg-amber-300", "bg-lime-300", "bg-teal-300", "bg-indigo-300", "bg-rose-300", "bg-cyan-300", "bg-fuschia-600", "bg-orange-500", "bg-red-500"]
 
     if (currentRow !== location.row) {
-        color = ["bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300"]
+        color = ["bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300", "bg-slate-300"]
     }
 
     return (
-        <div id={id} className={`${color[Math.min(location.row, 4)]} ` + "absolute p-1 border-2 border-neutral-900 rounded-full font-gilmer-bold -translate-x-1/2 -translate-y-1/2 float-left hover:scale-125"} style={{ left: `${percentX}%`, top: `${percentY}%` }} onClick={(e) => onClick(e)}></div>
+        <div id={id} className={`${color[Math.min(location.row, 8)]} ` + "absolute p-1 border-2 border-neutral-900 rounded-full font-gilmer-bold -translate-x-1/2 -translate-y-1/2 float-left hover:scale-125"} style={{ left: `${percentX}%`, top: `${percentY}%` }} onClick={(e) => onClick(e)}></div>
     )
 }
