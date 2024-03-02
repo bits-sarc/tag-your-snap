dec92ec7812c1b8161e8f79c3d5d9bf4d7108760
Himanshu Kumar <kumarh664@gmail.com>
2024-03-02 22:28:03 +0530

    block the website on phone

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -3,13 +3,32 @@ import NavBar from '../components/NavBar';
 import { Toaster } from 'react-hot-toast';
 
 export default function Root() {
+  const mobile = () => {
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    windowWidth = w.innerWidth || e.clientWidth || g.clientWidth; //window width
+
+    return windowWidth < 798;
+  }
+
   return (
-    <div className='bg-black text-white relative'>
-      <NavBar />
-      <div className="min-h-screen">
-        <Outlet />
-      </div>
-      <Toaster position="bottom-center" gutter={8} toastOptions={{ style: { background: "#363636", color: "#fff" } }} />
+    <div>
+      {mobile() ? (
+        <div className="bg-black text-white min-h-screen text-center">
+          Please open the website on your nearest desktop or laptop. <br />
+          Regards, SARC
+        </div>
+      ) : (
+        <div className='bg-black text-white relative'>
+          <NavBar />
+          <div className="min-h-screen">
+            <Outlet />
+          </div>
+          <Toaster position="bottom-center" gutter={8} toastOptions={{ style: { background: "#363636", color: "#fff" } }} />
+        </div>
+      ) }
     </div>
-  );
+  )
 }
