7e736ff4f8e06cc1955a14857db86995b8f2807a
Rakshit2622 <rakshit.sakhuja345@gmail.com>
2024-04-29 17:07:08 +0530

    excel import

--- server/scripts/export.py
+++ server/scripts/export.py
@@ -15,7 +15,7 @@ def export_bitsians():
         try:
             f.write(f"{i.branch_code} - {i.branch_name}\n\n\n")
             ws = wb.create_sheet(title=f"{i.branch_code}", index=c)
-            ws = wb.worksheets[c]h
+            ws = wb.worksheets[c]
             c += 1
             colms = Location.objects.filter(branc=i).distinct("row").count()
             ascii = 65
@@ -46,7 +46,7 @@ def export_bitsians():
             f.write("\n\n\n")
         except Exception as e:
             print(e)
-    
+
     f.close()
     wb.save("Exported_excel.xlsx")
     wb.close()
@@ -57,5 +57,40 @@ def export_bitsians():
     f.close()
 
 
+def export_excel():
+    branches = Branch.objects.all()
+    c = 0
+    wb = openpyxl.Workbook()
+    for i in branches:
+        try:
+            ws = wb.create_sheet(title=f"{i.branch_code}", index=c)
+            ws = wb.worksheets[c]
+            c += 1
+            colms = Location.objects.filter(branch=i).distinct("row").count()
+            ascii = 65
+            for j in range(0, colms):
+                if j == 0:
+                    ws[f"{chr(ascii)}1"] = "Sitting Row"
+                else:
+                    ws[f"{chr(ascii)}1"] = f"Standing Row {j}"
+                ascii += 1
+            ascii = 65
+            for l in range(0, colms):
+                locs = Location.objects.filter(Q(branch=i) & Q(row=l)).order_by("x")
+                row = 2
+                for k in locs:
+                    if k.tag:
+                        ws[f"{chr(ascii)}{row}"] = f"{k.tag.name}".title()
+                    else:
+                        ws[f"{chr(ascii)}{row}"] = ""
+                    row += 1
+                ascii += 1
+        except Exception as e:
+            print(e)
+
+    wb.save("Exported_excel.xlsx")
+    wb.close()
+
+
 def run():
-    export_bitsians()
+    export_excel()
