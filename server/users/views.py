888e6d071a52f52ab4c3330ca3df9ba1a57d8497
prithvi05prism <prithvi05prism@gmail.com>
2025-04-06 00:41:07 +0530

    debugging

--- server/users/views.py
+++ server/users/views.py
@@ -46,12 +46,9 @@ class GoogleView(APIView):
             algorithms=["HS256"],
         )
 
-        decode_jwt["bits_id"] = user.profile.bits_id
-        decode_jwt["branch"] = user.profile.branch.branch_code
-        
-        # if not (user.is_superuser or user.is_staff):
-        #      decode_jwt["bits_id"] = user.profile.bits_id
-        #      decode_jwt["branch"] = user.profile.branch.branch_code
+        if not (user.is_superuser or user.is_staff):
+             decode_jwt["bits_id"] = user.profile.bits_id
+             decode_jwt["branch"] = user.profile.branch.branch_code
         #     content = {
         #         "error": True,
         #         "message": "portal closed",
